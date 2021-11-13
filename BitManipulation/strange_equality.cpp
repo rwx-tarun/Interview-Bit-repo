f12e5b7ab66f831111fc6e43093843426760a10c
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-11-13 17:28:37 +0530

    Update strange_equality.cpp

--- BitManipulation/strange_equality.cpp
+++ BitManipulation/strange_equality.cpp
@@ -1,3 +1,16 @@
+// Problem Description
+
+// Given an integer A.
+// Two numbers X and Y are defined as follows:
+
+// X is the greatest number smaller than A such that XOR sum of X and A is the same as the sum of X and A.
+// Y is the smallest number greater than A such that XOR sum of Y and A is the same as the sum of Y and A.
+// Find and return the XOR of X and Y.
+
+// NOTE 1: XOR of X and Y is defined as X ^ Y where '^' is the BITWISE XOR operator.
+
+// NOTE 2: Your code will be run against a maximum of 100000 Test Cases.
+
 int Solution::solve(int A) {
     int  a =A;
     int cnt=0;
